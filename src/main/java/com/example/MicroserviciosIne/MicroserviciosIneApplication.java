4b89affab5a0eca52b7ea20bbfc58725fd61ce17
Gerardomm95 <gerardo_lide@hotmail.com>
2024-03-10 22:05:22 -0600

    Proyecto principal
    Se agrega proyecto principal

---
+++ src/main/java/com/example/MicroserviciosIne/MicroserviciosIneApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroserviciosIne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosIneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosIneApplication.class, args);
+	}
+
+}
